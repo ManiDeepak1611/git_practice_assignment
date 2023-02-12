e415a017267a070c1aa27842582ff78765a078d1
unknown <himanshugarg007@outlook.com>
2023-02-12 09:23:47 +0530

    done

--- palindrome1.js
+++ palindrome1.js
@@ -2,6 +2,8 @@ let str="nun";
 let bag="";
   for (let i=str.length-1;i>=0;i--){
       bag=bag+str[i];
+  }if(bag==str){
+    console.log("its a palindrome")
   }
   else {
       console.log("Not a palindrome");
